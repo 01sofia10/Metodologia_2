c438371b65aaf040b84c4adfca9ac3118695a4b9
01sofia10 <sofia.wu@edu.uah.es>
2025-03-26 13:45:09 +0100

    1.3

---
+++ src/MethodCallStack.java
@@ -0,0 +1,26 @@
+public class MethodCallStack {
+    public static void main(String[] args) {
+        System.out.println("main");
+        methodA();
+    }
+    public static void methodA() {
+        try {
+            System.out.println("A");
+            methodB();
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void methodB() throws ArithmeticException {
+        System.out.println("B");
+        methodC();
+    }
+    public static void methodC() throws ArithmeticException{
+        System.out.println("C");
+        methodD();
+    }
+    public static void methodD() throws ArithmeticException{
+        System.out.println("D");
+        System.out.println(1/0);
+    }
+}
